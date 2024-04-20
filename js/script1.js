eb65da7fe46fbff3b084ea9cd8a2014f876fba0e
nowrinfairuj <154911511+nowrinfairuj@users.noreply.github.com>
2024-04-21 00:59:11 +0600

    Add files via upload

---
+++ js/script1.js
@@ -0,0 +1,28 @@
+//wow
+ wow = new WOW(
+        {
+        boxClass:     'wow',      // default
+        animateClass: 'animated', // default
+        offset:       0,          // default
+        mobile:       true,       // default
+        live:         true        // default
+     }
+     )
+     wow.init();
+//curved text
+/**
+     * Demo 1
+     */
+    var arc = function (t)
+    {
+        return {
+            x: 500.0 + 300.0 * Math.cos(2.0 * Math.PI * t - 0.5 * Math.PI),
+            y: 400.0 + 300.0 * Math.sin(2.0 * Math.PI * t - 0.5 * Math.PI)
+        };
+    };
+
+    $('#text1').curvedText({
+        curve:    arc,
+        domain:   [-0.2, 0.2],
+        viewport: { x: 0.0, y: 0.0, width: 1000.0, height: 400.0 }
+    });
